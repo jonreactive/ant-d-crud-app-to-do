c513b2f9e6cec124a693ff1ddace37d74919f6c2
Jontronics <J.aquarone@gmail.com>
2018-10-16 12:36:09 -0400

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import ControlledInput from './components/ControlledInput';
-import RefsExampleOne from './components/RefsExampleOne';
 
 class App extends Component {
   
   render() {
     return (
       <div className="App">
-        <ControlledInput />
-        <hr />
-        <RefsExampleOne />
+        hello helo
       </div>
     );
   }
